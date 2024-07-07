ce308cb2c56f18e7662d3d8f96453877a9d9a164
Abhishek Singh <abhishek.skd13@gmail.com>
2024-07-07 16:08:10 +0530

    updated

--- js practice/practice-1.js
+++ js practice/practice-1.js
@@ -1,3 +1,23 @@
-let firstname = abhishek; 
-let lastname = singh;
- 
+// dialog boxes in javascript
+
+// alert ("hello")
+// const isconfirmed = confirm("are sure u want to proceed ?")
+// // const userInput = prompt ("please enter your name ")
+// console.log(isconfirmed)
+
+// template literals in javascript
+
+// const multi_string = `this is 
+//   multi string in javascript`;
+//   console.log(multi_string)
+
+// .length
+// .lengthuppercase()
+
+  
+// const message = 'hello! comostas'
+// const capitalMessage = message.toUpperCase()
+
+const faltuMessage = `     hi, i'am tikku ji     `
+faltuMessage.trim() 
+// So, basically it trims out extra spaces
