bc6a528a6ab7745cda2c5e336e2cac7f2daf2f8a
Abhishek Singh <abhishek.skd13@gmail.com>
2024-07-07 16:12:29 +0530

    message

--- js practice/practice-1.js
+++ js practice/practice-1.js
@@ -18,7 +18,7 @@
 // const message = 'hello! comostas'
 // const capitalMessage = message.toUpperCase()
 
-const faltuMessage = `     hi, i'am tikku ji     `
-const trimedMessage = faltuMessage.trim()
+// const faltuMessage = `     hi, i'am tikku ji     `
+// const trimedMessage = faltuMessage.trim()
 // faltuMessage.trim() 
 // So, basically it trims out extra spaces
